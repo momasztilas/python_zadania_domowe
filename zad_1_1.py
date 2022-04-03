10e5ba658f33d862b5299d542e32d14f6fc4c7c1
MomaszTilas <dumslejer666@gmail.com>
2022-04-03 21:05:34 +0200

    zadanie 1 ziemniaki i banany

--- zad_1_1.py
+++ zad_1_1.py
@@ -2,9 +2,9 @@
 ziemniaków. Niech program policzy i wyświetli, ile trzeba będzie zapłacić za pięć kilo
 ziemniaków."""
 
-cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
-zaplata = cena_ziemniaka * 5
-print(f'5 kg ziemniaków kosztuje {zaplata:.2f} zł.')
+# cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
+# zaplata = cena_ziemniaka * 5
+# print(f'5 kg ziemniaków kosztuje {zaplata:.2f} zł.')
 
 """Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
 kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli, ile trzeba będzie
@@ -20,9 +20,16 @@ chce kupić. Niech program policzy i wyświetli, ile trzeba będzie zapłacić z
 banany razem. I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za
 banany czy za ziemniaki."""
 
-# cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
-# waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
-# cena_banana = float(input("Podaj cenę kilograma bananów: "))
-# waga_banana = float(input("Ile bananów chcesz kupić? (kg): "))
-#
-# print(f'Twoje zakupy kosztują {(cena_ziemniaka * waga_ziemniaka) + (cena_banana * waga_banana)} zł.')
+cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków (zł): "))
+waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
+cena_banana = float(input("Podaj cenę kilograma bananów (zł): "))
+waga_banana = float(input("Ile bananów chcesz kupić? (kg): "))
+zaplata_ziemniak = cena_ziemniaka * waga_ziemniaka
+zaplata_banan = cena_banana * waga_banana
+print(f'Twoje zakupy kosztują {(zaplata_ziemniak + zaplata_banan):.2f} zł.')
+if zaplata_ziemniak > zaplata_banan:
+    print(f'Więcej zapłacisz za ziemniaki, {zaplata_ziemniak:.2f} zł.')
+elif zaplata_banan > zaplata_ziemniak:
+    print(f'Więcej zapłacisz za banany, {zaplata_banan:.2f} zł.')
+else:
+    print('Banany i ziemniaki kosztują po równo.')
