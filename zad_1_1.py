96d8dbfbabea24fe68019e150c7e0e841d1ea55f
MomaszTilas <dumslejer666@gmail.com>
2022-04-03 20:50:11 +0200

    zadanie 1 ziemniaki 5kg

--- zad_1_1.py
+++ zad_1_1.py
@@ -1,15 +1,18 @@
 """Napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje kilo
 ziemniaków. Niech program policzy i wyświetli, ile trzeba będzie zapłacić za pięć kilo
-ziemniaków.
+ziemniaków."""
 
+cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
+zaplata = cena_ziemniaka * 5
+print(f'5 kg ziemniaków kosztuje {zaplata:.2f} zł.')
 
-Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
+"""Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
 kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli, ile trzeba będzie
 zapłacić za te ziemniaki."""
 
-cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
-waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
-print(f'Twoje ziemniaki kosztują {cena_ziemniaka * waga_ziemniaka} zł.')
+# cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
+# waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
+# print(f'Twoje ziemniaki kosztują {cena_ziemniaka * waga_ziemniaka} zł.')
 
 """Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
 kilo ziemniaków, ile kilo ziemniaków chce kupić, ile kosztuje kilo bananów i ile kilo bananów
@@ -17,3 +20,9 @@ chce kupić. Niech program policzy i wyświetli, ile trzeba będzie zapłacić z
 banany razem. I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za
 banany czy za ziemniaki."""
 
+# cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
+# waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
+# cena_banana = float(input("Podaj cenę kilograma bananów: "))
+# waga_banana = float(input("Ile bananów chcesz kupić? (kg): "))
+#
+# print(f'Twoje zakupy kosztują {(cena_ziemniaka * waga_ziemniaka) + (cena_banana * waga_banana)} zł.')
