8fd8ec1f700af5ada5d74c68201707f4d5c3091e
MomaszTilas <dumslejer666@gmail.com>
2022-04-03 20:39:09 +0200

    zadanie 1 ziemniaki

---
+++ zad_1_1.py
@@ -0,0 +1,19 @@
+"""Napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje kilo
+ziemniaków. Niech program policzy i wyświetli, ile trzeba będzie zapłacić za pięć kilo
+ziemniaków.
+
+
+Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
+kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli, ile trzeba będzie
+zapłacić za te ziemniaki."""
+
+cena_ziemniaka = float(input("Podaj cenę kilograma ziemniaków: "))
+waga_ziemniaka = float(input("Ile ziemniaków chcesz kupić? (kg): "))
+print(f'Twoje ziemniaki kosztują {cena_ziemniaka * waga_ziemniaka} zł.')
+
+"""Potem napisz program, który prosi użytkownika (przez input() ), żeby podał, ile kosztuje
+kilo ziemniaków, ile kilo ziemniaków chce kupić, ile kosztuje kilo bananów i ile kilo bananów
+chce kupić. Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki i
+banany razem. I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za
+banany czy za ziemniaki."""
+
