7982b312a8df96a581e29ce9ecab7ffb291f9006
MomaszTilas <dumslejer666@gmail.com>
2022-04-07 22:33:57 +0200

    zadanie 12 - statystyka

---
+++ zad_2_3.py
@@ -0,0 +1,42 @@
+"""
+Napisz program, który odczytuje od użytkownika wiele liczb.
+Program powinien wyliczyć i na końcu wypisać następujące statystyki:
+
+liczba podanych liczb (ile sztuk),
+suma,
+średnia,
+minimum
+maksimum
+
+NIE używaj funkcji wbudowanych!
+
+"""
+
+import statistics
+import math
+liczby = []
+i = 1
+
+while i <= 10:
+    liczba = int(input(f'Wprowadź liczbę nr {i}: '))
+    i += 1
+    liczby.append(liczba)
+def mymax(liczby):
+    max = liczby[0]
+    for x in liczby:
+        if x > max:
+            max = x
+    return max
+def mymin(liczby):
+    min = liczby[0]
+    for y in liczby:
+        if y < min:
+            min = y
+    return min
+
+print (f'Wprowadziłeś {len(liczby)} liczb.')
+print (f'Suma wprowadzonych liczb to {math.fsum(liczby)}')
+print (f'Średnia wprowadzonych liczb to {statistics.mean(liczby)}')
+print (f'Najmniejsza wprowadzona liczba to {mymin(liczby)}')
+print (f'Największa wprowadzona liczba to {mymax(liczby)}')
+
